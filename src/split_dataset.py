7117960b03f27c98eade71749ea22b031610983b
Anna <annajs@student.ubc.ca>
2023-05-07 12:34:13 -0700

    Updated code for the creation of the train/dev/test datasets. 	- Now there are specific python files for each dataset (presently St̓át̓imcets and Gitksan) in order to break up their data in a way that is specific to their story breakdown. 	- There is also a file for common functions used in this process regardless of language.

--- src/split_dataset.py
+++ src/split_dataset.py
@@ -1,80 +1,36 @@
-from os import getcwd, walk
+# *** Common functions for creating train/dev/tests datasets ***
 
-# Returns a train, dev, and test dataset
-def create_datasets():
-    speaker_1_folder_path = "/plaintext/BS/"
-    speaker_1_sentences = read_all_files_for_speaker(speaker_1_folder_path)
-    print(f"Speaker 1: {len(speaker_1_sentences)} sentences.")
-
-    speaker_2_folder_path = "/plaintext/HH/"
-    speaker_2_sentences = read_all_files_for_speaker(speaker_2_folder_path)
-    print(f"Speaker 2: {len(speaker_2_sentences)} sentences.")
-
-    speaker_3_folder_path = "/plaintext/VG/"
-    speaker_3_sentences = read_all_files_for_speaker(speaker_3_folder_path)
-    print(f"Speaker 3: {len(speaker_3_sentences)} sentences.")
-
-    # Total amount of sentences in the data = 1294
-    # Because speaker 2 has the least data available, that will be our test set
-    # Training set is then speaker 1 + speaker 3 = 924
-    # We want ~20% for dev = 185 sentences
-    # Probably best to group data by story (more realistic?), so we'll find a story cutoff close to that
-    # Dev data = last six stories from speaker 3 (starts from sentence 299)
-
-    train = speaker_1_sentences + speaker_3_sentences[:298]
-    print("Train data:", len(train), "sentences.")
-    dev = speaker_3_sentences[298:]
-    print("Dev data:", len(dev), "sentences.")
-    test = speaker_2_sentences
-    print("Test data:", len(test), "sentences.")
-
-    print("Finished splitting the three datasets.")
-    return train, dev, test
-
-# Read in each file from this speaker, and return all the example sentences stored in one big list
-def read_all_files_for_speaker(folder_path):
-    sentences = []
-    for root, dirs, files in walk(getcwd() + folder_path):
-        for file in files:
-            if file.endswith(".txt"):
-                sentences.extend(read_file(getcwd() + folder_path + file)[:])
-          
-    return sentences
-
-# Returns a list of examples (where each example is a list containing the 5 lines)
-def read_file(file_path):
+# Returns a list of examples (where each example is a list containing the transcription, seg, etc. lines)
+def read_file(file_path, lines_per_example):
     with open(file_path) as f:
         lines = f.readlines()
-        lines.append("\n") # So that the final sentence also ends in a blank line
+        # To handle inconsistencies in file formatting - add a new line if the file doesn't already end in one 
+        if not lines[-1].endswith("\n"):
+            # The new line ensures the final example can processed in the same way as all the previous examples
+            lines[-1] = lines[-1] + "\n"
+            lines.append("\n")
 
     sentences = []
     current_sentence = []
     for i, line in enumerate(lines):
-        if (i + 1) % 6 == 0: # Every 6th line is a blank line marking the end of the current sentence
+         # After each example is a blank line marking the end of the current sentence
+         # Whenever we get there, it's time to add the present example to our list
+        if (i + 1) % (lines_per_example + 1) == 0:
             sentences.append(current_sentence)
             current_sentence = []
         else:
             current_sentence.append(line)
-    # The final sentence needs to end with a newline, for consistency
-    sentences[-1][-1] = sentences[-1][-1] + "\n"
+
     return(sentences)
 
 def create_file_of_sentences(list, file_name):
     file = open(file_name, "w")
-    for example in list:
+    for i, example in enumerate(list):
         if len(example) > 0:
-            for i, line in enumerate(example):
+            for j, line in enumerate(example):
                 file.write(line)
-                if i >= len(example) - 1: # Final line
+                # Add blank line after each example (but not if it's the end of the dataset, bc that results in a double newline at EOF)
+                if (j >= len(example) - 1) and (i < len(list) - 1):
                     file.write("\n")
     file.close()
-
-def main():
-    # Read the files and break them down into the three sets
-    train, dev, test = create_datasets()
-    create_file_of_sentences(train, "train.txt")
-    create_file_of_sentences(dev, "dev.txt")
-    create_file_of_sentences(test, "test.txt")
-
-if __name__ == '__main__':
-    main()
+    
---
+++ src/split_statimcets_data.py
@@ -0,0 +1,103 @@
+from os import getcwd, walk
+from split_dataset import create_file_of_sentences, read_file
+
+# Returns a train, dev, and test dataset
+def create_datasets():
+    stories = [None] * 18
+    current_story = []
+    folder_path = "/Data/statimcets/"
+    lines_per_example = 4
+
+    # We want to read in the sentences while maintaning the story structure,
+    # so that we can divide the data up by story
+    for root, dirs, files in walk(getcwd() + folder_path):
+        for file_name in files:
+            if file_name.startswith("story") and file_name.endswith(".txt"):
+
+                # Read in all the sentences
+                current_story.extend(read_file(getcwd() + folder_path + file_name, lines_per_example)[:])
+                
+                # Get the story number so we know which story this is
+                # (It doesn't read the stories in order so we need to order them manually)
+                file_number = int(file_name[6:-4]) - 1
+
+                # Insert the story at the correct numerical pos in our list of stories
+                stories.pop(file_number)
+                stories.insert(file_number, current_story)
+
+                # Reset
+                current_story = []
+
+    for i, story in enumerate(stories):
+        print(f"Story #{i + 1} has {len(story)} sentences.")
+
+    # Total amount of sentences in the data = 977
+    # The breakdown by story is as follows:
+    # story 1: 114
+    # story 2: 53
+    # story 3: 24
+    # story 4: 73
+    # story 5: 59
+    # story 6: 25
+    # story 7: 52
+    # story 8: 32
+    # story 9: 46
+    # story 10: 67
+    # story 11: 37
+    # story 12: 66
+    # story 13: 30
+    # story 14: 16
+    # story 15: 74
+    # story 16: 79
+    # story 17: 47
+    # story 18: 83
+    # We want a 60%/20%/20% train/dev/test split
+    # Train goal: 586 sentences, 11 stories
+    # Dev goal: 195 sentences, 4 stories
+    # Test goal: 195 sentences, 4 stories
+    # DEV: Stories 17, 18, 11, 6 = 192 sentences, 4 stories
+    # TEST: Stories 2, 3, 4, 9 = 196 sentences, 4 stories
+    # TRAIN: the rest of the stories = 589 sentences, 10 stories
+
+    # Assemble the stories into the three sets
+    train, dev, test = [], [], []
+    train.extend([stories[0]])
+    train.extend([stories[4]])
+    train.extend(stories[6:8])
+    train.extend([stories[9]])
+    train.extend(stories[11:16])
+    sentence_count = 0
+    for story in train:
+        sentence_count += len(story)
+    print("Train data:", len(train), "stories containing", sentence_count, "sentences.")
+    
+    dev = [stories[5]] + [stories[10]] + stories[16:]
+    sentence_count = 0
+    for story in dev:
+        sentence_count += len(story)
+    print("Dev data:", len(dev), "stories containing", sentence_count, "sentences.")
+    
+    test = stories[1:4] + [stories[8]]
+    test_sentence_count = 0
+    for story in test:
+        test_sentence_count += len(story)
+    print("Test data:", len(test), "stories containing", test_sentence_count, "sentences.")
+
+    return train, dev, test
+
+# Converts from a list of stories to just a list of examples, then calls create_file_of_sentences
+def create_file_of_sentences_helper(list, file_name):
+    list_of_sentences = []
+    for story in list:
+        list_of_sentences.extend(story)
+    create_file_of_sentences(list_of_sentences, file_name)
+
+def main():
+    # Read the files and break them down into the three sets
+    train, dev, test = create_datasets()
+    create_file_of_sentences_helper(train, "train.txt")
+    create_file_of_sentences_helper(dev, "dev.txt")
+    create_file_of_sentences_helper(test, "test.txt")
+
+if __name__ == '__main__':
+    main()

374bfe70619e6f8fde86def795b2e4532e45c4f0
Anna <annajs@student.ubc.ca>
2024-03-12 18:59:40 -0700

    Fixed bug where data paths hadn't been updated in a shell script.

--- src/prepare_seg.sh
+++ src/prepare_seg.sh
@@ -7,6 +7,6 @@ PIPELINE_SET=data/test.txt
 
 # Prepare the data for fairseq
 python3 src/preprocess_seg.py --train_file=$TRAIN_SET --dev_file=$DEV_SET --test_file=$TEST_SET --pipeline_file=$PIPELINE_SET
-fairseq-preprocess --source-lang input --target-lang output --trainpref train --validpref dev --destdir data-bin/
+fairseq-preprocess --source-lang input --target-lang output --trainpref generated_data/train --validpref generated_data/dev --destdir data-bin/
 # Train fairseq
 sh src/train_seg.sh 

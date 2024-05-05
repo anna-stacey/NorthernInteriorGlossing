16498afaf5dfe831570649ae9aef5120feb6ec24
Anna <annajs@student.ubc.ca>
2024-05-05 15:33:25 -0700

    Updated pipeline code to work with new OOL handling. 	- Also refactored it so that it just reads in the new formatted seg output, rather than the fairseq output!  So it doesn't need to use any segmentation methods! Yay.

--- src/dev_pipeline.sh
+++ src/dev_pipeline.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SEG_INPUT=generated_data/pipeline.input
 SEG_OUTPUT=generated_data/pipeline_fairseq.output
+SEG_PREDICTIONS=generated_data/seg_pred.txt
 GLOSS_TRAIN_SET=data/train.txt
 GLOSS_DEV_SET=data/dev.txt
 GLOSS_TEST_SET=data/dev.txt
@@ -10,4 +11,4 @@ GLOSS_LINE_NUMBER=3 # 3 for St̓át̓imcets, 4 for Gitksan
 # Run the segmentation
 cat $SEG_INPUT | fairseq-interactive --path models/checkpoint_best.pt data-bin > $SEG_OUTPUT  
 # Run the glossing
-python3 src/pipeline.py --seg_output_file=$SEG_OUTPUT --gloss_train_file=$GLOSS_TRAIN_SET --gloss_dev_file=$GLOSS_DEV_SET --gloss_test_file=$GLOSS_TEST_SET --segmentation_line_number=$SEG_LINE_NUMBER --gloss_line_number=$GLOSS_LINE_NUMBER
+python3 src/pipeline.py --seg_pred_file=$SEG_PREDICTIONS --gloss_train_file=$GLOSS_TRAIN_SET --gloss_dev_file=$GLOSS_DEV_SET --gloss_test_file=$GLOSS_TEST_SET --segmentation_line_number=$SEG_LINE_NUMBER --gloss_line_number=$GLOSS_LINE_NUMBER
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,8 +1,7 @@
 import click
 import re
-from test_seg import read_lines_from_file, format_fairseq_output, reassemble_predicted_line
 from gloss import evaluate_system, extract_X_and_y, format_X_and_y, make_sentence_list_with_prediction, read_datasets, reassemble_predicted_words, train_system, write_output_file, LEFT_INFIX_BOUNDARY, RIGHT_INFIX_BOUNDARY, LEFT_REDUP_INFIX_BOUNDARY, RIGHT_REDUP_INFIX_BOUNDARY, REGULAR_BOUNDARY, REDUPLICATION_BOUNDARY
-from glossed_data_utilities import read_file
+from glossed_data_utilities import add_back_OOL_words, handle_OOL_words, read_file
 
 GOLD_OUTPUT_FILE_NAME = "pipeline_gold.txt"
 PRED_OUTPUT_FILE_NAME = "pipeline_pred.txt"
@@ -20,6 +19,7 @@ def reassemble_sentences(word_list, word_count_by_sentence):
 
     return sentence_list
 
+# Input: a list of segmentation lines, as strings
 def remove_infix_boundary_errors(seg_line_list):
     updated_seg_line_list = []
     for seg_line in seg_line_list:
@@ -38,29 +38,36 @@ def remove_infix_boundary_errors(seg_line_list):
     return updated_seg_line_list
 
 @click.command()
-@click.option("--seg_output_file", help = "The name of the output file from the segmentation.")
+@click.option("--seg_pred_file", help = "The name of the predicted file from the segmentation process.")
 @click.option("--gloss_train_file", help = "The name of the file containing all sentences in the train set.")
 @click.option("--gloss_dev_file", help = "The name of the file containing all sentences in the dev set.")
 @click.option("--gloss_test_file", help = "The name of the file containing all sentences in the test set.")
 @click.option("--segmentation_line_number", help = "The line that contains the segmented sentence.  For example if there are four lines each and the segmentation is the second line, this will be 2.")
 @click.option("--gloss_line_number", help = "The line that contains the glossed sentence.  For example if there are four lines each and the gloss is the third line, this will be 3.")
-def main(seg_output_file, gloss_train_file, gloss_dev_file, gloss_test_file, segmentation_line_number, gloss_line_number):
+def main(seg_pred_file, gloss_train_file, gloss_dev_file, gloss_test_file, segmentation_line_number, gloss_line_number):
     # Convert right away to prevent off-by-one errors
     gloss_line_number = int(gloss_line_number) - 1
     segmentation_line_number = int(segmentation_line_number) - 1
 
-    # Read in the fairseq output (segmented words)
-    seg_output = read_lines_from_file(seg_output_file)
-    # Process the gross fairseq output format so we just have the words
-    seg_output = format_fairseq_output(seg_output)
-    # Put the seg lines back together
-    seg_output = reassemble_predicted_line(read_file(gloss_test_file), seg_output)
+    # First, let's read in the original train and test sets
+    train, throwaway, test = read_datasets(gloss_train_file, gloss_dev_file, gloss_test_file)
+
+    # Next, get the predicted seg lines.  These will be our input.
+    seg_output = (sentence[1] for sentence in read_file(seg_pred_file))
     # For now... remove single infix markers (incorrect!)
     seg_output = remove_infix_boundary_errors(seg_output)
-    test_X = seg_output
-    # Ok! Now we have our input prepped. Now let's get our gold standard glosses
-    train, throwaway, test = read_datasets(gloss_train_file, gloss_dev_file, gloss_test_file)
-    throwaway, test_y = extract_X_and_y(test, segmentation_line_number, gloss_line_number)
+    # Modify our test set to contain the *predicted* seg lines
+    test = make_sentence_list_with_prediction(test, seg_output, 1)
+    # Now we have the original training set,
+    # and a test set with the correct X and y for the pipeline.
+
+    # Save the test set as-is for output-printing purposes
+    original_test = test
+    original_test_transcription_lines = list(sentence[0] for sentence in original_test)
+
+    train, test = handle_OOL_words([train, test])[:2]
+
+    test_X, test_y = extract_X_and_y(test, segmentation_line_number, gloss_line_number)
 
     # Keep versions with boundaries for word-by-word evaluation
     test_X_with_boundaries = test_X
@@ -73,14 +80,14 @@ def main(seg_output_file, gloss_train_file, gloss_dev_file, gloss_test_file, seg
 
     # Now we can format the input and output for glossing
     test_X, test_y = format_X_and_y(test_X, test_y)
-    # print(len(test_X), len(test_y))
     pred_y = evaluate_system(test_X, test_y, test_X_with_boundaries, test_y_with_boundaries, crf, stem_dict)
 
     # Create output files for the sigmorphon evaluation
     isOpenTrack = True # Make this true if you want to see the segmentation output, too
     # Assemble output file of predictions
-    test_with_predictions = make_sentence_list_with_prediction(test, seg_output, segmentation_line_number)
-    test_with_predictions = make_sentence_list_with_prediction(test_with_predictions, reassemble_predicted_words(pred_y), gloss_line_number)
+    test_with_predictions = make_sentence_list_with_prediction(original_test, seg_output, segmentation_line_number)
+    pred_y_to_print = add_back_OOL_words(original_test_transcription_lines, reassemble_predicted_words(pred_y))
+    test_with_predictions = make_sentence_list_with_prediction(test_with_predictions, pred_y_to_print, gloss_line_number)
     write_output_file(test_with_predictions, PRED_OUTPUT_FILE_NAME, segmentation_line_number, gloss_line_number, isOpenTrack)
     # And create a file of the gold version, formatted the same way to permit comparison
     write_output_file(test, GOLD_OUTPUT_FILE_NAME, segmentation_line_number, gloss_line_number, isOpenTrack)
